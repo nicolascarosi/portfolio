fc850210e9758ad11da49e1eb65ebe66c57b6b2a
Nicolás Carosi <64372950+nicolascarosi@users.noreply.github.com>
2023-03-23 15:36:24 -0300

    new copy hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,14 +1,23 @@
-import { profile } from 'assets'
-import { Wrapper } from 'utils'
-import { StyledHero, StyledHeroContainer } from './Hero.styles'
+import { profile } from "assets";
+import { Wrapper } from "utils";
+import { StyledHero, StyledHeroContainer } from "./Hero.styles";
 
 const Hero = () => (
   <StyledHero id="about-me">
     <Wrapper>
-      <StyledHeroContainer className="square-before" data-animation="fade-in-left">
+      <StyledHeroContainer
+        className="square-before"
+        data-animation="fade-in-left"
+      >
         <div className="info">
-          <h1>Front End development using modern technologies and simple design.</h1>
-          <p>I'm Nicolas and I'm currently working as a front end engineer at Sofredigital. I hold a degree in Technical Programing and I've been working on important projects as you can find below in my portfolio.</p>
+          <h1>
+            Front End development using modern technologies and simple design.
+          </h1>
+          <p>
+            I'm Nicolas and I'm currently working as a front end engineer at
+            Kala. I hold a degree in Technical Programing and I've been working
+            on important projects as you can find below in my portfolio.
+          </p>
         </div>
       </StyledHeroContainer>
       <div className="image" data-animation="fade-in-right">
@@ -16,6 +25,6 @@ const Hero = () => (
       </div>
     </Wrapper>
   </StyledHero>
-)
+);
 
-export { Hero }
+export { Hero };
